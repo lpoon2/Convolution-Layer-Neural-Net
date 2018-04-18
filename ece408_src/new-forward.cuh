049f2369b76f5c770778a4a59ef63ad04623c21e
Larry <poonl@nextcapital.com>
2018-04-17 19:29:21 -0500

    0.1 accuracy

--- ece408_src/new-forward.cuh
+++ ece408_src/new-forward.cuh
@@ -1,11 +1,13 @@
 
 #ifndef MXNET_OPERATOR_NEW_FORWARD_CUH_
 #define MXNET_OPERATOR_NEW_FORWARD_CUH_
-#define TILE_WIDTH 16
+#define TILE_WIDTH 32
 
 
 
 #include <mxnet/base.h>
+#include <stdio.h>
+#include <math.h>
 
 namespace mxnet
 {
@@ -14,43 +16,35 @@ namespace op
 
 __global__ void forward_kernel(float *y, const float *x, const float *k, const int B, const int M, const int C, const int H, const int W, const int K)
 {
-
-    /*
-    Modify this function to implement the forward pass described in Chapter 16.
-    We have added an additional dimension to the tensors to support an entire mini-batch
-    The goal here is to be correct AND fast.
-    We have some nice #defs for you below to simplify indexing. Feel free to use them, or create your own.
-    */
-
     const int H_out = H - K + 1;
     const int W_out = W - K + 1;
-    int W_grid=W_out/TILE_WIDTH;
-    //int H_grid=H_out/TILE_WIDTH;
 
+    #define y4d(i3, i2, i1, i0) y[(i3) * (M * H_out * W_out) + (i2) * (H_out * W_out) + (i1) * (W_out) + i0]
+    #define x4d(i3, i2, i1, i0) x[(i3) * (C * H * W) + (i2) * (H * W) + (i1) * (W) + i0]
+    #define k4d(i3, i2, i1, i0) k[(i3) * (C * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
 
-// An example use of these macros:
-// float a = y4d(0,0,0,0)
-// y4d(0,0,0,0) = a
-#define y4d(i3, i2, i1, i0) y[(i3) * (M * H_out * W_out) + (i2) * (H_out * W_out) + (i1) * (W_out) + i0]
-#define x4d(i3, i2, i1, i0) x[(i3) * (C * H * W) + (i2) * (H * W) + (i1) * (W) + i0]
-#define k4d(i3, i2, i1, i0) k[(i3) * (C * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
-int n = blockIdx.x;
-int m = blockIdx.y;
-int h = blockIdx.z/W_grid+threadIdx.y;
-int w = blockIdx.z%W_grid+threadIdx.x;
-float acc=0;
-for(int c=0;c<C;c++){
-  for(int p=0;p<K;p++) {
-    for(int q=0; q<K; q++){
-      acc+=x4d(n,c,h+p,w+q)*k4d(m,c,p,q);
+    int b = (blockIdx.y * blockDim.y + threadIdx.y) / H_out;
+    //int b = blockIdx.y;
+    int m = (blockIdx.x * blockDim.x + threadIdx.x) / W_out;
+    //int m = blockIdx.x;
+    int h = (blockIdx.y * blockDim.y + threadIdx.y) % H_out;
+    int w = (blockIdx.x * blockDim.x + threadIdx.x) % W_out;
+
+    if (((blockIdx.x * blockDim.x + threadIdx.x) < (W_out * C)) && ((blockIdx.y * blockDim.y + threadIdx.y) < (H_out * B))) {
+      float acc=0;
+      for(int c=0;c<C;c++) {
+        for(int p=0;p<K;p++) {
+          for(int q=0; q<K; q++) {
+            acc+=x4d(b,c,h+p,w+q)*k4d(m,c,p,q);
+          }
+        }
+      }
+      y4d(b,m,h,w)=acc;
     }
-  }
-}
-y4d(n,m,h,w)=acc;
-//y4d(blockIdx.x,blockIdx.y,threadIdx.x,threadIdx.y)=sum;
-#undef y4d
-#undef x4d
-#undef k4d
+    //y4d(blockIdx.x,blockIdx.y,threadIdx.x,threadIdx.y)=sum;
+    #undef y4d
+    #undef x4d
+    #undef k4d
 }
 
 /*
@@ -61,13 +55,9 @@ y4d(n,m,h,w)=acc;
 template <>
 void forward<gpu, float>(mshadow::Tensor<gpu, 4, float> &y, const mshadow::Tensor<gpu, 4, float> &x, const mshadow::Tensor<gpu, 4, float> &w)
 {
-
     // Use mxnet's CHECK_EQ to do assertions.
     // Remove this assertion when you do your implementation!
     //CHECK_EQ(0, 1) << "Remove this line and replace with your implementation";
-    cudaStream_t s = y.stream_->stream_;
-    // Extract the tensor dimensions into B,M,C,H,W,K
-    // ...
     const int B = x.shape_[0]; //batches
     const int M = y.shape_[1]; //output channels
     const int C = x.shape_[1]; //input channels
@@ -77,14 +67,11 @@ void forward<gpu, float>(mshadow::Tensor<gpu, 4, float> &y, const mshadow::Tenso
     // Set the kernel dimensions
     const int H_out = H - K + 1;
     const int W_out = W - K + 1;
-    int W_grid=W_out/TILE_WIDTH;
-    int H_grid=H_out/TILE_WIDTH;
-    int Z=H_grid*W_grid;
-     dim3 gridDim(B,M,Z);
-     dim3 blockDim(TILE_WIDTH,TILE_WIDTH,1);
+    //int Z=H_grid*W_grid;
+    dim3 gridDim(ceil((M*W_out)/TILE_WIDTH*1.0), ceil((B*H_out)/TILE_WIDTH*1.0), 1);
+    dim3 blockDim(TILE_WIDTH,TILE_WIDTH,1);
     // Call the kernel
-    forward_kernel<<<gridDim, blockDim, 0, s>>>(y.dptr_,x.dptr_,w.dptr_, B,M,C,H,W,K);
-
+    forward_kernel<<<gridDim, blockDim>>>(y.dptr_,x.dptr_,w.dptr_, B,M,C,H,W,K);
     // Use MSHADOW_CUDA_CALL to check for CUDA runtime errors.
     MSHADOW_CUDA_CALL(cudaDeviceSynchronize());
 
