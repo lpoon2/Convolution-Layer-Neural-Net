643443a11e2229ff48b1e8d0e2a774c83ffed56e
Gerald Kozel <littlegkozel@gmail.com>
2018-05-10 16:40:41 -0500

    very slight speed up by changing loading of W

--- ece408_src/new-forward.cuh
+++ ece408_src/new-forward.cuh
@@ -1,57 +1,92 @@
-
 #ifndef MXNET_OPERATOR_NEW_FORWARD_CUH_
 #define MXNET_OPERATOR_NEW_FORWARD_CUH_
-#define TILE_WIDTH 32
+#define TILE_WIDTH 16
 
 #include <mxnet/base.h>
-#include <stdio.h>
-#include <math.h>
 
 namespace mxnet
 {
 namespace op
 {
+__global__ void forward_kernel(float * __restrict__ y, const float * __restrict__ x, const float * __restrict__ k, const int B, const int M, const int C, const int H, const int W, const int K){
 
-__global__ void forward_kernel(float *y, const float *x, const float *k, const int B, const int M, const int C, const int H, const int W, const int K)
-{
+    float sum=0.0;
     const int H_out = H - K + 1;
     const int W_out = W - K + 1;
-
+    int X_width = (TILE_WIDTH+K-1);
+    extern __shared__ float shmem[];
+    float* X_share = &shmem[0];
+    float* W_share = &shmem[(X_width*X_width)];
     #define y4d(i3, i2, i1, i0) y[(i3) * (M * H_out * W_out) + (i2) * (H_out * W_out) + (i1) * (W_out) + i0]
     #define x4d(i3, i2, i1, i0) x[(i3) * (C * H * W) + (i2) * (H * W) + (i1) * (W) + i0]
     #define k4d(i3, i2, i1, i0) k[(i3) * (C * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
 
-    int b = (blockIdx.y * blockDim.y + threadIdx.y) / H_out;
-    //int b = blockIdx.y;
-    int m = (blockIdx.x * blockDim.x + threadIdx.x) / W_out;
-    //int m = blockIdx.x;
-    int h = (blockIdx.y * blockDim.y + threadIdx.y) % H_out;
-    int w = (blockIdx.x * blockDim.x + threadIdx.x) % W_out;
+    int W_grid = ceil(W_out/(TILE_WIDTH*1.0));
+    int H_grid=ceil(H_out/(TILE_WIDTH*1.0));
 
-    if (((blockIdx.x * blockDim.x + threadIdx.x) < (W_out * M)) && ((blockIdx.y * blockDim.y + threadIdx.y) < (H_out * B))) {
-      float acc=0;
-      for(int c=0;c<C;c++) {
-        for(int p=0;p<K;p++) {
-          for(int q=0; q<K; q++) {
-            acc+=x4d(b,c,h+p,w+q)*k4d(m,c,p,q);
-          }
+    int b = blockIdx.x;
+    int m = blockIdx.y;
+    int h0 = threadIdx.y;
+    int w0 = threadIdx.x;
+    int h_base = (blockIdx.z / W_grid)*TILE_WIDTH;
+    int w_base = (blockIdx.z % W_grid)*TILE_WIDTH;
+    int h = h_base+h0;
+    int w = w_base+w0;
+
+    for(int c=0;c<C;c++){
+
+      if((h0<K) && (w0<K)){
+        W_share[(threadIdx.x*K)+threadIdx.y]=k4d(m,c,w0,h0); //corner turning - switch indexing to vertical
+      }
+      __syncthreads();
+
+      for(int i=h; i< h_base + X_width ;i+=TILE_WIDTH){
+        for(int j=w; j< w_base + X_width;j+=TILE_WIDTH){
+          X_share[(i-h_base)*X_width+(j-w_base)]=x4d(b,c,i,j); //load in tile needed for shared memory
         }
       }
-      y4d(b,m,h,w)=acc;
-    }
-    //y4d(blockIdx.x,blockIdx.y,threadIdx.x,threadIdx.y)=sum;
+      __syncthreads();
+
+      sum+=(X_share[((threadIdx.y)*X_width)+(threadIdx.x)]*W_share[0]
+          + X_share[((threadIdx.y)*X_width)+(threadIdx.x+1)]*W_share[1]
+          + X_share[((threadIdx.y)*X_width)+(threadIdx.x+2)]*W_share[2]
+          + X_share[((threadIdx.y)*X_width)+(threadIdx.x+3)]*W_share[3]
+          + X_share[((threadIdx.y)*X_width)+(threadIdx.x+4)]*W_share[4]
+          + X_share[((threadIdx.y+1)*X_width)+(threadIdx.x)]*W_share[(K)]
+          + X_share[((threadIdx.y+1)*X_width)+(threadIdx.x+1)]*W_share[(K)+1]
+          + X_share[((threadIdx.y+1)*X_width)+(threadIdx.x+2)]*W_share[(K)+2]
+          + X_share[((threadIdx.y+1)*X_width)+(threadIdx.x+3)]*W_share[(K)+3]
+          + X_share[((threadIdx.y+1)*X_width)+(threadIdx.x+4)]*W_share[(K)+4]
+          + X_share[((threadIdx.y+2)*X_width)+(threadIdx.x)]*W_share[(2*K)]
+          + X_share[((threadIdx.y+2)*X_width)+(threadIdx.x+1)]*W_share[(2*K)+1]
+          + X_share[((threadIdx.y+2)*X_width)+(threadIdx.x+2)]*W_share[(2*K)+2]
+          + X_share[((threadIdx.y+2)*X_width)+(threadIdx.x+3)]*W_share[(2*K)+3]
+          + X_share[((threadIdx.y+2)*X_width)+(threadIdx.x+4)]*W_share[(2*K)+4]
+          + X_share[((threadIdx.y+3)*X_width)+(threadIdx.x)]*W_share[(3*K)]
+          + X_share[((threadIdx.y+3)*X_width)+(threadIdx.x+1)]*W_share[(3*K)+1]
+          + X_share[((threadIdx.y+3)*X_width)+(threadIdx.x+2)]*W_share[(3*K)+2]
+          + X_share[((threadIdx.y+3)*X_width)+(threadIdx.x+3)]*W_share[(3*K)+3]
+          + X_share[((threadIdx.y+3)*X_width)+(threadIdx.x+4)]*W_share[(3*K)+4]
+          + X_share[((threadIdx.y+4)*X_width)+(threadIdx.x)]*W_share[(4*K)]
+          + X_share[((threadIdx.y+4)*X_width)+(threadIdx.x+1)]*W_share[(4*K)+1]
+          + X_share[((threadIdx.y+4)*X_width)+(threadIdx.x+2)]*W_share[(4*K)+2]
+          + X_share[((threadIdx.y+4)*X_width)+(threadIdx.x+3)]*W_share[(4*K)+3]
+          + X_share[((threadIdx.y+4)*X_width)+(threadIdx.x+4)]*W_share[(4*K)+4]);
+      __syncthreads();
+  }
+
+  if ( (b < B) && (m < M) && (h < H_out) && (w < W_out)){
+    y4d(b,m,h,w)=sum;
+  }
+
     #undef y4d
     #undef x4d
     #undef k4d
 }
 
-/*
-   This function is called by new-inl.h
-   Any code you write should be executed by this function.
-   For ECE408, we only expect the float version of the operator to be called, so here we specialize with only floats.
-*/
+
 template <>
-void forward<gpu, float>(mshadow::Tensor<gpu, 4, float> &y, const mshadow::Tensor<gpu, 4, float> &x, const mshadow::Tensor<gpu, 4, float> &w)
+void forward<gpu, float>(mshadow::Tensor<gpu, 4, float> & __restrict__ y, const mshadow::Tensor<gpu, 4, float> & __restrict__ x, const mshadow::Tensor<gpu, 4, float> & __restrict__ w)
 {
     const int B = x.shape_[0]; //batches
     const int M = y.shape_[1]; //output channels
@@ -59,24 +94,24 @@ void forward<gpu, float>(mshadow::Tensor<gpu, 4, float> &y, const mshadow::Tenso
     const int H = x.shape_[2]; //height of input
     const int W = x.shape_[3]; //width of input
     const int K = w.shape_[3]; //height and width of weights
-    // Set the kernel dimensions
     const int H_out = H - K + 1;
     const int W_out = W - K + 1;
-    int X = ceil((M*W_out)/(TILE_WIDTH*1.0));
-    int Y = ceil((B*H_out)/(TILE_WIDTH*1.0));
-    dim3 gridDim(X, Y, 1);
+    //cudaStream_t s = y.stream_->stream_;
+    const int W_grid = ceil(W_out/(1.0*TILE_WIDTH));
+    const int H_grid = ceil(H_out/(1.0*TILE_WIDTH));
+    const int Z = W_grid*H_grid;
+
+    //printf("HELLO======= B :%d, M: %d, C: %d, H:%d, W:%d, K:%d", B, M, C, H, W, K);
+    dim3 gridDim(B, M, Z);
     dim3 blockDim(TILE_WIDTH,TILE_WIDTH,1);
-    // Call the kernel
-    forward_kernel<<<gridDim, blockDim>>>(y.dptr_,x.dptr_,w.dptr_, B,M,C,H,W,K);
+
+    size_t shmem_size=sizeof(float)*(((TILE_WIDTH + K - 1) * (TILE_WIDTH + K - 1)) + (K * K));
+    forward_kernel<<<gridDim, blockDim,shmem_size>>>(y.dptr_, x.dptr_,w.dptr_, B, M, C, H, W, K);
     // Use MSHADOW_CUDA_CALL to check for CUDA runtime errors.
     MSHADOW_CUDA_CALL(cudaDeviceSynchronize());
 
 }
 
-/*
-    This tells mxnet how to do an op when it's not a float.
-    This is not used in the ECE408 project
-*/
 template <typename gpu, typename DType>
 void forward(mshadow::Tensor<gpu, 4, DType> &y, const mshadow::Tensor<gpu, 4, DType> &x, const mshadow::Tensor<gpu, 4, DType> &w)
 {
